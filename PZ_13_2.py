8eb089f0a37e6c6f4dd51bd7572fd4c400254db6
likergi <aleksandrkaruna7@gmail.com>
2025-10-01 15:48:18 +0300

    Update PZ_13_2.py

--- PZ_13_2.py
+++ PZ_13_2.py
@@ -1,6 +1,9 @@
+#В двумерном списке найти отрицательные элементы, сформировать из них новый 
+#массив. Вывести размер полученного массива.
 from PZ_13_1 import matrix
 
 negative_elements = [num for row in matrix for num in row if num < 0]
 
 print("\nВсе отрицательные элементы матрицы:", negative_elements)
+
 print("Размер полученного массива:", len(negative_elements))
